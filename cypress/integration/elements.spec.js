49e481b5aac9c25dc90d7548d5a6fbdf45810747
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-08 19:15:22 -0300

    Texts

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,11 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Work with basic elements', () => {
-    it('Text', () => {
+    it.skip('Text', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
         cy.get('body').should('contain', 'Cuidado');
         cy.get('span').should('contain','Cuidado');
-        cy.get('.facilAchar')
-            .should('contain',' Cuidado')
+        cy.get('.facilAchar').should('contain',' Cuidado')
+    })
+
+    it.only('Links..', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text','Voltou!')
+
+        cy.reload() // Refres na Tela
+        cy.get('#resultado').should('have.not.text','Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text','Voltou!')
     })
 })
