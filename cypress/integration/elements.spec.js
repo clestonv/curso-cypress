4cd31fc4f16089483e2ab370f4b00631be24a597
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-09 22:26:05 -0300

    Textos

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -12,7 +12,7 @@ describe('Work with basic elements', () => {
     it('Text', () => {        
         cy.get('body').should('contain', 'Cuidado');
         cy.get('span').should('contain','Cuidado');
-        cy.get('.facilAchar').should('contain',' Cuidado')
+        cy.get('.facilAchar').should('contain','Cuidado')
     })
 
     it('Links..', () => {        
@@ -23,5 +23,29 @@ describe('Work with basic elements', () => {
         cy.get('#resultado').should('have.not.text','Voltou!')
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text','Voltou!')
+    })
+
+    // Textos
+    
+    it.only('TextFields', () => {
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('Textarea')
+            .should('have.value', 'Textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('?????')
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay: 100})
+            .should('have.value','acerto')
+
     })
 })
