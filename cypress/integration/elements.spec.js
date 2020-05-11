4adba76c53fc53626410ee71be6b4c4e1c12f88f
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-10 22:03:56 -0300

    Mutiplo Combos

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -73,7 +73,7 @@ describe('Work with basic elements', () => {
 
     })
     // Combobox
-    it.only('Combobox ..', () => {
+    it('Combobox ..', () => {
         cy.get('[data-test=dataEscolaridade]')
             .select('2o grau completo')
             .should('have.value','2graucomp')
@@ -81,5 +81,14 @@ describe('Work with basic elements', () => {
         cy.get('[data-test=dataEscolaridade]')
             .select('1graucomp')
             .should('have.value','1graucomp')
+            //TODO validar opções do combo
     })
+    // Mutiplo Combo
+    it.only('Combo Multiplo', () => {
+        cy.get('[data-testid=dataEsportes]')
+            .select(['natacao','Corrida','nada'])
+        
+            //TODO validar opções selecionadas do combo mutiplo
+    })
+    
 })
