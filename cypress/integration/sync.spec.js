4443e350e230f15df5b4ec2bb2cdb8bf25008a52
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-12 22:55:54 -0300

    Wai e Timeout

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -35,6 +35,21 @@ describe('Esperas', ()=> {
             .should('contain','Item 2')
         
     })
+
+    it.only('Uso do timeout', ()=> {
+        // cy.get('#buttonDelay').click()
+        // cy.get('#novoCampo', {timeout: 1000}).should('exist') // Controlando o Timeout do Cypress
+        
+        // cy.get('#buttonListDOM').click()
+        // // cy.wait(5000) Usar em casos extremos
+        // cy.get('#lista li span', {timeout: 30000}) // O tempo que ele espera, se a condição for atendida ele passa
+        //     .should('contain','Item 2')
+        cy.get('#buttonListDOM').click()
+        cy.get('#lista li span', {timeout: 30000})
+            .should('have.length',1) // Verificar o tamanho da lista
+        cy.get('#lista li span')
+            .should('have.length',2)
+    })
 })
 
 
