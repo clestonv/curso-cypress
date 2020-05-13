5d7cfed67c6d8061ce6da9d456e2649fb7e2be52
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-12 22:19:27 -0300

    Retrys

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -16,6 +16,14 @@ describe('Esperas', ()=> {
         cy.get('#novoCampo').should('exist') 
         cy.get('#novoCampo').type('Funciona')
     })
+
+    it.only('Deve fazer retrys',()=> {  
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo')
+            .should('not.exist')
+            .should('exist') 
+       
+    })
 })
 
 
