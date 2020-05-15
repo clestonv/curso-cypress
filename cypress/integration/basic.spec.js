ecbb9cd503ec0a94e679407509d671f7dbc9c183
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-14 22:37:29 -0300

    Reutilizando Titulo

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -12,11 +12,29 @@ describe('Cypress Basics', () => {
         cy.title()
             .should('be.equal','Campo de Treinamento')
             .and('contain','Campo de Treinamento').debug()
+        
+            let syncTitle
 
-        cy.title().should(title => {
+        cy.title().then(title => {
             console.log(title)
+
+            cy.get('#formNome').type(title)
+
+            syncTitle = title
+        })
+
+        cy.get('[data-cy=dataSobrenome]').then($el => {
+            $el.val(syncTitle)
+        })
+
+        cy.get('#elementosForm\\:sugestoes').then($el => {
+            cy.wrap($el).type(syncTitle)
         })
     })
+
+    
+
+    
 
     it('Should find and interact with an element ..', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
