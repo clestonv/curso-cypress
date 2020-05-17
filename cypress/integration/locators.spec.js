66bc2544132eda9419bebb3acac6543fe5a0ead6
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-17 20:45:54 -0300

    Plugin Xpath

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -14,7 +14,11 @@ describe('Validando Mensagems ...', () => {
         cy.get('[onclick*=\'Francisco\']').click() // Crackeando com contra barras \\
         cy.get("[onclick*='Francisco']").click() // Crackeando com aspas duplas ""
         cy.get('#tabelaUsuarios td:contains(\'Doutorado\'):eq(0) ~td:eq(3)')
-        cy.get('#tabelaUsuarios tr:contains(\'Doutorado\):eq(0) td:eq(6) input')
+        cy.get('#tabelaUsuarios tr:contains(\'Doutorado\'):eq(0) td:eq(6) input')
+    })
+
+    it('Using xpath',() => {
+        cy.xpath('//input')
     })
 
 })
