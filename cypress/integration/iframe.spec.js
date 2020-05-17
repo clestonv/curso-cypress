b3f5572a3059d05f711e83d66c94ab199be45daa
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-17 16:06:57 -0300

    Iframe 2

--- cypress/integration/iframe.spec.js
+++ cypress/integration/iframe.spec.js
@@ -8,8 +8,17 @@ describe('Work with iframe', () => {
             const body = iframe.contents().find('body')
             cy.wrap(body).find('#tfield')
                 .type('Funciona?')
-                .should('have.value','Funciona?')
+                .should('have.value','Funciona?')           
         })
+    })
+
+    it('Deve testar frame diretamente', () => {
+        cy.visit('https://wcaquino.me/cypress/frame.html')
+        cy.get('#otherButton').click()
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Click OK!')
+        })
+        
     })
     
 })
