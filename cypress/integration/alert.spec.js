cf9e2dedf40f56ac092489aa8044f78ebd646bc6
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-16 22:23:04 -0300

    Alert

---
+++ cypress/integration/alert.spec.js
@@ -0,0 +1,17 @@
+describe('Work with alerts', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html') // Hooks
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Alert',() => {
+        cy.get('#alert').click()
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Alert Simples')
+        })
+    })
+})
