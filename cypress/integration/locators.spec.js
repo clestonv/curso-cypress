2215cdc964576f962a6b1974ece985f186c38d2f
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-17 22:34:20 -0300

    Locator Concluido

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -18,7 +18,9 @@ describe('Validando Mensagems ...', () => {
     })
 
     it('Using xpath',() => {
-        cy.xpath('//input')
+        cy.xpath('//input[contains(@onclick, \'Francisco\')]')
+        cy.xpath("//table[@id='tabelaUsuarios']//td[contains(., 'Francisco')]/..//input[@type='text']")
+        cy.xpath("//td[contains(., 'Usuario A')]/following-sibling::td[contains(., 'Mestrado')]/..//input[@type='text']").type('Funciona')
     })
 
 })
