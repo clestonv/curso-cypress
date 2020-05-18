c9d37740b2ae5e9049784ae7c619700a3374116e
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-17 23:50:13 -0300

    Each

--- cypress/integration/dinamic.spec.js
+++ cypress/integration/dinamic.spec.js
@@ -19,5 +19,21 @@ describe('Dinamics Tests', () => {
         })
     })
 
+    it.only('Deve selecionar todos usando Each', () => {
+        cy.get('#formNome').type('Usuario')
+        cy.get('[data-cy=dataSobrenome]').type('Qualquer')
+        cy.get(`[name=formSexo][value=F]`).click()
+        cy.get('[name=formComidaFavorita]').each($el => {
+            // $el.click()
+            if($el.val() != 'vegetariano')
+            cy.wrap($el).click()
+        })
+        cy.get('#formEscolaridade').select('Doutorado')
+        cy.get('#formEsportes').select('Corrida')
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)').should('have.text','Cadastrado!')
+        // cy.clickAlert('#formCadastrar','Tem certeza que voce eh vegetariano?')
+    })
+
     
 })
