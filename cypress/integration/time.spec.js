ec8a366aa95e07e795c459465d492f2cec8f3d56
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-18 00:08:44 -0300

    Tick

--- cypress/integration/time.spec.js
+++ cypress/integration/time.spec.js
@@ -18,4 +18,24 @@ describe('Dinamics Tests', () => {
         cy.get('#buttonNow').click()
         cy.get('#resultado > span').should('contain','10/04/2012')
     })
+
+    it.only('Goes to the future', () => {
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain','15897')
+        cy.get('#resultado > span').invoke('text').should('gt',1589770891184)
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 0)
+        // cy.wait(1000)
+        // cy.get('#buttonTimePassed').click()
+        // cy.get('#resultado > span').invoke('text').should('lte', 1000)
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 5000)
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 15000)
+    })
 })
