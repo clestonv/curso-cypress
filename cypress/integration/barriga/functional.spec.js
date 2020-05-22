769c347bf9cfb928aa3da9f2a4b302fb01da8bdb
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-22 19:52:07 -0300

    Inserir Conta

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -23,12 +23,20 @@ describe('Work with alerts', () => {
     it('Should update an account', () => {
         // cy.get(':nth-child(1) > :nth-child(2) > .fa-edit')
         cy.acessarMenuConta()
-        
+
         cy.xpath(loc.conta.xp_btn_alterar).click()
         cy.get(loc.conta.nome)
             .clear()
             .type('Conta alterada')
         cy.get(loc.conta.btn_salvar).click()
         cy.get(loc.message).should('contain','Conta atualizada com sucesso!')
+    })
+
+    it('Should not create an account with same name', () => {
+        cy.acessarMenuConta()
+
+        cy.get(loc.conta.nome).type('Conta alterada')
+        cy.get(loc.conta.btn_salvar).click()
+        cy.get(loc.message).should('contain','code 400')
     })
 })
