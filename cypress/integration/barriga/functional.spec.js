fe2ed5e64dc81f0cc5b57828ecab7ead8f9d0682
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-21 22:11:08 -0300

    Organizando locators

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,31 +1,33 @@
 /// <reference types="cypress" />
 
+import loc from '../../support/locators'
+
 describe('Work with alerts', () => {
     before(() => {
         cy.visit('https://barrigareact.wcaquino.me') // Hooks
-        cy.get('[data-test=email]').type('cleberson.osorioti@hotmail.com')
-        cy.get('[data-test=passwd]').type('T@bl3tenis')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('contain','Bem vindo')
+        cy.get(loc.login.user).type('cleberson.osorioti@hotmail.com')
+        cy.get(loc.login.password).type('T@bl3tenis')
+        cy.get(loc.login.btn_login).click()
+        cy.get(loc.message).should('contain','Bem vindo')
     })
 
     it('Should create an account', ()=> {
-        cy.get('[data-test=menu-settings] > .fas').click()
-        cy.get('[href="/contas"]').click()
-        cy.get('[data-test=nome]').type('Conta Teste')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('contain','Conta inserida com sucesso!')
+        cy.get(loc.menu.settings).click()
+        cy.get(loc.menu.contas).click()
+        cy.get(loc.conta.nome).type('Conta Teste')
+        cy.get(loc.conta.btn_salvar).click()
+        cy.get(loc.message).should('contain','Conta inserida com sucesso!')
     })
 
     it('Should update an account', () => {
         // cy.get(':nth-child(1) > :nth-child(2) > .fa-edit')
-        cy.get('[data-test=menu-settings] > .fas').click()
-        cy.get('[href="/contas"]').click()
-        cy.xpath("//table//td[contains(., 'Conta Teste')]/..//i[@class='far fa-edit']").click()
-        cy.get('[data-test=nome]')
+        cy.get(loc.menu.settings).click()
+        cy.get(loc.menu.contas).click()
+        cy.xpath(loc.conta.xp_btn_alterar).click()
+        cy.get(loc.conta.nome)
             .clear()
             .type('Conta alterada')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('contain','Conta atualizada com sucesso!')
+        cy.get(loc.conta.btn_salvar).click()
+        cy.get(loc.message).should('contain','Conta atualizada com sucesso!')
     })
 })
---
+++ cypress/support/locators.js
@@ -0,0 +1,19 @@
+const locators = {
+    login: {
+        user: '[data-test=email]',
+        password: '[data-test=passwd]',
+        btn_login:'.btn',
+    },
+    menu: {
+        settings:'[data-test=menu-settings] > .fas',
+        contas:'[href="/contas"]',
+    },
+    conta: {
+        nome: '[data-test=nome]',
+        btn_salvar: '.btn',
+        xp_btn_alterar: "//table//td[contains(., 'Conta Teste')]/..//i[@class='far fa-edit']",
+    },
+    message:'.toast-message',
+}
+
+export default locators;
