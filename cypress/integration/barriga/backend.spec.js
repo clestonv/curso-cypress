37e7e3a92bd4fbc0d7c07067f85e34c809bc505c
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-24 22:09:17 -0300

    Inserindo Conta

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -8,7 +8,7 @@ describe('Work with alerts', () => {
     beforeEach(()=> {
        
     })
-
+https://barrigarest.wcaquino.me/contas
     it('Should create an account', ()=> {
        cy.request({
            method: 'POST',
@@ -19,6 +19,23 @@ describe('Work with alerts', () => {
                 senha: "T@bl3tenis",
            },
        }).its('body.token').should('not.be.empty')
+       .then(token => {
+            cy.request({
+                url: 'https://barrigarest.wcaquino.me/contas',
+                method: 'POST',
+                headers:{
+                    Authorization: `JWT ${token}`
+                },
+                body: {
+                    nome: 'Conta via Rest',
+                }
+            }).as('response')
+       })  
+    cy.get('@response').then(res => {
+        expect(res.status).to.be.equal(201)
+        expect(res.body).to.have.property('id')
+        expect(res.body).to.have.property('nome','Conta via Rest')
+    })
     })
 
     it('Should update an account', () => {
