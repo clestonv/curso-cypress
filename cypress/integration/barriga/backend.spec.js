dc1020ace0e25edce5c3017116e3c50f169b6dbc
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-26 18:45:35 -0300

    Inserir movimentação vi Rest

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -36,32 +36,23 @@ describe('Work with alerts', () => {
     })
 
     it('Should update an account', () => {
-        cy.request({
-            method: 'GET',
-            url: '/contas',
-            headers:{
-                Authorization: `JWT ${token}` // Inserindo o token
-            },
-            qs: {
-                nome: 'Conta para alterar'
-            },
-        }).then(res => {
+        cy.getContaByName('Conta para alterar')
+        .then(contaId => {
             cy.request({
-                url: `/contas/${res.body[0].id}`,
+                url: `/contas/${contaId}`,
                 method: 'PUT',
                 headers:{
-                     Authorization: `JWT ${token}` // Inserindo o token
-                 },
-                 body: {
-                     nome: 'Conta Aleterada via Rest',
-                 }            
+                        Authorization: `JWT ${token}` // Inserindo o token
+                    },
+                    body: {
+                        nome: 'Conta Aleterada via Rest',
+                    }            
             }).as('response')
         })       
-
        cy.get('@response').its('status').should('be.equal', 200)
     })
 
-    it.only('Should not create an account with same name',() => {
+    it('Should not create an account with same name',() => {
         cy.request({
             url: '/contas',
             method: 'POST',
@@ -79,6 +70,31 @@ describe('Work with alerts', () => {
             expect(res.status).to.be.equal(400)
             expect(res.body.error).to.be.equal('Já existe uma conta com esse nome!')
         })
+    })
+
+    it('Should create a transaction',() => {
+        cy.getContaByName('Conta para movimentacoes')
+        .then(contaId => {
+            cy.request({
+                method: 'POST',
+                url: '/transacoes',
+                headers:{
+                    Authorization: `JWT ${token}` // Inserindo o token
+                },
+                body: {
+                    conta_id: contaId,
+                    data_pagamento: Cypress.moment().add({days: 1}).format('DD/MM/YYYY'),
+                    data_transacao: Cypress.moment().format('DD/MM/YYYY'),
+                    descricao: "Des",
+                    envolvido: "inter",
+                    status: true,
+                    tipo: "REC",
+                    valor: "123",
+                }
+            }).as('response')
+        })
+        cy.get('@response').its('status').should('to.be.equal', 201)
+        cy.get('@response').its('body.id').should('exist')
     })
 
     it('Should get balance', () => {        
