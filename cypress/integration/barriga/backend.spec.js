2b3c8a3c3df59d747de581308ceaa765206ccae0
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-05-26 21:29:47 -0300

    Saldo via Rest

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -97,8 +97,62 @@ describe('Work with alerts', () => {
         cy.get('@response').its('body.id').should('exist')
     })
 
-    it('Should get balance', () => {        
+    it.only('Should get balance', () => {        
+       cy.request({
+           url: '/saldo',
+           method: 'GET',
+           headers:{
+            Authorization: `JWT ${token}` // Inserindo o token
+        },
+       }).then(res => {
+           let saldoConta = null
+           res.body.forEach(c => {
+               if(c.conta === 'Conta para saldo' )saldoConta = c.saldo
+           })
+           expect(saldoConta).to.be.equal('534.00')
+       })
+
+       cy.request({
+           method: 'GET',
+           url: '/transacoes',
+           headers:{
+            Authorization: `JWT ${token}` // Inserindo o token
+           },
+           qs: {
+               descricao: 'Movimentacao 1, calculo saldo'
+           }
+       }).then(res => {
+            cy.request({
+                url: `/transacoes/${res.body[0].id}`,
+                method: 'PUT',
+                headers:{
+                Authorization: `JWT ${token}` // Inserindo o token
+                },
+                body: {
+                    status: true,
+                    data_transacao: Cypress.moment(res.body[0].data_transacao).format('DD/MM/YYYY'),
+                    data_pagamento: Cypress.moment(res.body[0].data_pagamento).format('DD/MM/YYYY'),
+                    descricao: res.body[0].descricao,
+                    envolvido: res.body[0].envolvido,
+                    valor: res.body[0].valor,
+                    conta_id: res.body[0].conta_id,
+                }
+            })
+       })  
        
+       cy.request({
+            url: '/saldo',
+            method: 'GET',
+            headers:{
+            Authorization: `JWT ${token}` // Inserindo o token
+            },
+        }).then(res => {
+            let saldoConta = null
+            res.body.forEach(c => {
+                if(c.conta === 'Conta para saldo' )saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('4034.00')
+        })
     })
 
     it('Should remova a transaction', () => {
